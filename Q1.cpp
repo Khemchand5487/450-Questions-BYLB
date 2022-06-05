9642504cb4c36aa0620606665e34750b61142e03
Khem Chand <khemchand5487@gmail.com>
2022-06-05 14:26:56 +0530

    Create Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,11 @@
+// Reverse a String or array
+
+string reverseWord(string str){
+    
+    int n= str.size();
+    for(int i=0; i<n/2;i++) {
+      swap(str[i], str[n-i-1]);
+    }
+    
+    return str;
+}
