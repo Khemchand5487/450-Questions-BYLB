484292e2b2c0649d019c0b13eedb2a7f20bce298
Khem Chand <khemchand5487@gmail.com>
2022-06-12 18:35:35 +0530

    Merge Without Extra Space

---
+++ Q12.cpp
@@ -0,0 +1,23 @@
+// Merge Without Extra Space
+// link: https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays5135/1
+
+void merge(int arr1[], int arr2[], int n, int m) {
+    int gap = ceil((float)(n+m)/2);
+    while(gap) {
+        int i=0, j=gap;
+        while(j<m+n) {
+            if(j<n && arr1[i] > arr1[j]) swap(arr1[i], arr1[j]);
+            else if(i<n && j>=n && arr1[i] > arr2[(j-n)]) {
+                swap(arr1[i], arr2[(j-n)]);
+                i++, j++;
+            } 
+            else if(i>=n && j>=n && arr2[i-n] > arr2[j-n]){
+                swap(arr2[i-n], arr2[j-n]);
+                i++, j++;
+            }
+            else i++, j++;
+        }
+        if(gap==ceil((float)gap/2)) break;
+        gap = ceil((float)gap/2);
+    }
+}
